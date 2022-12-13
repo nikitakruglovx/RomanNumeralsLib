05adb62e647078c959d9b32a0c6875eb2599cb2f
nikitakruglovx <useraccountx@protonmail.com>
2022-12-13 20:39:31 +0300

    Release v.1.0

--- README.md
+++ README.md
@@ -5,8 +5,31 @@
     <img src="https://i.imgur.com/Q0Kklv5.jpg" height="160">
 </p>
 
+1. Convert XXII Roman numerals to Arabic numerals
+2. Convert 123 Arabic numerals to Roman numerals
+
 # Usage:
 
+Linked RomanNumerals header file
 ```C++
 #include "RomanNumerals.h"
+```
+Then create an object of the RomanNumerals class
+```C++
+RomanNumerals Number;
+```
+There are two methods available to you
+
+![img](https://i.imgur.com/5q1pgTE.jpg)
+
+# Example
+```C++
+#include <iostream>
+#include "RomanNumerals.h"
+
+int main() {
+    RomanNumerals Number;
+    int OutputNumber = Number.fromRoman("MMXXII");
+    std::cout << OutputNumber << std::endl;
+}
 ```
--- RomanNumerals.h
+++ RomanNumerals.h
@@ -18,11 +18,18 @@ private:
 	char M = 'M';
 
 	std::map <char, int> dataNumFromRoman;
-	std::map <int, std::string> dataNumToRoman;
 
 public:
-	void toRoman(int r) {
+	std::string toRoman(int N) {
 
+		if (N >= 1000) { return "M" + toRoman(N - 1000); }
+		if (N >= 500) { return N >= 900 ? ("CM" + toRoman(N - 900)) : ("D" + toRoman(N - 500)); }
+		if (N >= 100) { return N >= 400 ? ("CD" + toRoman(N - 400)) : ("C" + toRoman(N - 100)); }
+		if (N >= 50) { return N >= 90 ? ("XC" + toRoman(N - 90)) : ("L" + toRoman(N - 50)); }
+		if (N >= 10) { return N >= 40 ? ("XL" + toRoman(N - 40)) : ("X" + toRoman(N - 10)); }
+		if (N >= 5) { return N == 9 ? "IX" : "V" + toRoman(N - 5); }
+		if (N > 0) { return N == 4 ? "IV" : "I" + toRoman(N - 1); }
+		return "";
 	}
 
 	int fromRoman(std::string N) {
@@ -38,7 +45,7 @@ public:
 		int ch = 0;
 		int result = 0;
 
-		for (const char c : N) {
+		for (auto c : N) {
 			int N = dataNumFromRoman[c];
 
 			if (N < ch) {
